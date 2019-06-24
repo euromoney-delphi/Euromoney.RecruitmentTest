3be9eed26cccb361c7f1c2bb4bc2c3a37207a517
Shweta Pawar <shweta_pawar@hotmail.co.uk>
2019-06-24 22:40:54 +0100

    coding test completed

--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ContentConsole
 {
@@ -6,39 +7,75 @@ namespace ContentConsole
     {
         public static void Main(string[] args)
         {
-            string bannedWord1 = "swine";
-            string bannedWord2 = "bad";
-            string bannedWord3 = "nasty";
-            string bannedWord4 = "horrible";
+
 
             string content =
                 "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
 
-            int badWords = 0;
-            if (content.Contains(bannedWord1))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord2))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord3))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord4))
-            {
-                badWords = badWords + 1;
-            }
+            DataStore dtStore = new DataStore();
 
-            Console.WriteLine("Scanned the text:");
-            Console.WriteLine(content);
-            Console.WriteLine("Total Number of negative words: " + badWords);
+            if (args.Count() > 0)
+            {
+                switch (args[0])
+                {
+                    case "setBannedWords":
+                        dtStore.BannedWords = args[1].Split(',').ToList();
+                        ShowResult(content, dtStore, true);
+                        break;
+
+                    case "unfiltered":
+                        ShowResult(content, dtStore, false);
+                        break;
+
+                    case "filtered":
+                        ShowResult(content, dtStore, true);
+                        break;
+
+                    case "/help":
+                    case "/?":
+                        DisplayHelp();
+                        break;
+                }
+            }
+            else
+            {
+                DisplayHelp();
+            }
 
             Console.WriteLine("Press ANY key to exit.");
             Console.ReadKey();
         }
+
+        private static void ShowResult(string content, DataStore dtStore, bool filtered)
+        {
+            ContentFilter contentFilter = new ContentFilter(dtStore.BannedWords); ;
+            if (filtered)
+            {
+                string filteredContent = contentFilter.FilterContent(content);
+                Console.WriteLine(filteredContent);
+            }
+            else
+            {
+                Console.WriteLine(content);
+            }
+
+            int badWordsCount = contentFilter.GetBadWordsCount(content);
+            Console.WriteLine("Total Number of negative words: " + badWordsCount);
+        }
+
+        public static void DisplayHelp()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Content Filter");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Use below arguments:");
+            Console.WriteLine("  setBannedWords : resets the banned word list. Please enter comma seperated list of words in quotes");
+            Console.WriteLine("  filtered : resets the filtered content");
+            Console.WriteLine("  unfiltered : Shows the unfiltered text, with the word count");
+            Console.WriteLine("");
+        }
+
     }
 
 }
