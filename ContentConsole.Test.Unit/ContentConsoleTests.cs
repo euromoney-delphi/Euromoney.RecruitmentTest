380f60fa372c693809d24bfd4dc7a8567728e40f
Chris Butcher <butchercd@yahoo.co.uk>
2019-07-15 21:04:30 +0100

    Fix Issues With Test Adapter Missing

---
+++ ContentConsole.Test.Unit/ContentConsoleTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace ContentConsole.Test.Unit
+{
+    [TestFixture]
+    class ContentConsoleTests
+    {
+        [Test]
+        public void PositiveTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
