1be1fdc6f3efa1f9af15ea1fa7d89348c7a015d7
fanthonysamy <53081959+fanthonysamy@users.noreply.github.com>
2019-07-19 11:22:40 +0100

    Add files via upload

---
+++ ContentConsole.Test.Unit/NegationTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ContentConsole.Test.Unit
+{
+    public class NegationTests
+    {
+        [Test]
+        public void GetTotalCount()
+        {
+            string content =
+                "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            string result = Program.GetTotalNumberOfBadWords(content);
+
+            Assert.AreEqual("Total count of bad words : 2", result);
+        }
+
+        [Test]
+        public void FilterBadWords()
+        {
+            string content =
+                "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            string result = Program.FilterBadWords(content);
+
+            Assert.AreEqual("The weather in Manchester in winter is b#d. It rains all the time - it must be h#######e for people visiting.", result);
+        }
+
+        [Test]
+        public void DisableBadFiltering()
+        {
+            var content =
+                "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            string result = Program.DisableBadFiltering(content);
+            string count = Program.GetTotalNumberOfBadWords(content);
+
+            Assert.AreEqual("Total count of bad words : 2The weather in Manchester in winter is . It rains all the time - it must be  for people visiting.", count + result);
+        }
+    }
+}
--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ContentConsole
 {
@@ -6,6 +10,7 @@ namespace ContentConsole
     {
         public static void Main(string[] args)
         {
+
             string bannedWord1 = "swine";
             string bannedWord2 = "bad";
             string bannedWord3 = "nasty";
@@ -14,6 +19,7 @@ namespace ContentConsole
             string content =
                 "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
 
+            #region
             int badWords = 0;
             if (content.Contains(bannedWord1))
             {
@@ -31,14 +37,116 @@ namespace ContentConsole
             {
                 badWords = badWords + 1;
             }
+            #endregion
 
             Console.WriteLine("Scanned the text:");
             Console.WriteLine(content);
-            Console.WriteLine("Total Number of negative words: " + badWords);
+            Console.WriteLine("Total Number of Bad words: " + badWords);
 
             Console.WriteLine("Press ANY key to exit.");
+
+           string total = GetTotalNumberOfBadWords(content);
+           Console.WriteLine(total);
+
+           string filter = FilterBadWords(content);
+           Console.WriteLine(filter);
+
+           string disable = DisableBadFiltering(content);
+           Console.WriteLine(total + disable);
+
+           UpdateBadWord(GetBadWords());
+
             Console.ReadKey();
+
+            /*
+             * Different approach
+             * Repository Pattern
+             * 1. Create an Interface an implement below methods in the repository
+             * 2. Access the repository from the controller and return the results.
+             */
+        }
+
+        public static List<BadWordModel> GetBadWords()
+        {
+            List<BadWordModel> BadWords = new List<BadWordModel>
+            {
+                new BadWordModel {BadWord = "swine"},
+                new BadWordModel {BadWord = "bad"},
+                new BadWordModel {BadWord = "nasty"},
+                new BadWordModel {BadWord = "horrible"}
+            };
+
+            return BadWords;
+        }
+
+        public static string GetTotalNumberOfBadWords(string sentence)
+        {
+            int count = 0;
+            string output = "Total count of bad words : ";
+
+            foreach (var BadWord in GetBadWords())
+            {
+                if (sentence.ToLower().Contains(BadWord.BadWord))
+                {
+                    count++;
+                }
+            }
+
+            return output + count;
+        }
+
+        public static string FilterBadWords(string sentence)
+        {
+            string strReplace = string.Empty;
+
+            foreach (var word in GetBadWords().Select(c=>c.BadWord))
+            {
+                string first = word.First().ToString();
+                string last = word.Last().ToString();
+
+                string removeLast = word.Remove(word.Length - 1);
+                string wordToRemove = removeLast.Remove(0, 1);
+
+                if (sentence.ToLower().Contains(word))
+                {
+                    for (int i = 0; i < wordToRemove.Length; i++)
+                    {
+                        strReplace += "#";
+                    }
+
+                    sentence = Regex.Replace(sentence, word, first + strReplace + last, RegexOptions.IgnoreCase);
+                }
+            }
+
+            return sentence;
+        }
+
+        public static string DisableBadFiltering(string sentence)
+        {
+            foreach (var BadWordModel in GetBadWords())
+            {
+                sentence = Regex.Replace(sentence,
+                    "\\b" + string.Join("\\b|\\b", BadWordModel.BadWord) + "\\b", "");
+            }
+
+
+            return sentence;
+        }
+
+        public static void UpdateBadWord(List<BadWordModel> BadWordEntity)
+        {
+            if (BadWordEntity != null)
+            {
+                foreach (var badWordModel in BadWordEntity)
+                {
+                    badWordModel.BadWord = "test";
+                }
+            }
         }
     }
 
+    public class BadWordModel
+    {
+        public string BadWord { get; set; }
+    }
 }
