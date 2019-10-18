61cb01e3b94860e9f3c88d34d3cab1fe21ec40d8
Hamza Rana <HAMZARANA@TFL.GOV.UK>
2019-10-18 02:45:36 +0100

    fixed story # 4

--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -11,20 +11,15 @@ namespace ContentConsole
         {
             const string content = "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
             Console.WriteLine("Scanned the text:");
-          
 
-            if (args.Length == 0 || args[0].ToLower() != "-disablefilter")
-            {
-                var badWordsRepository = new FilterWordsRepository();
-                var textAnalysisService = new TextFilterService(new FilterWordsProvider(badWordsRepository.LoadBadWordsFromRepo(ConfigSettings.FilterBadWordsRepoFilePath)));
-                var result = textAnalysisService.AnalyseText(content);
-                Console.WriteLine(result.FilteredContent);
-                Console.WriteLine("Total Number of negative words: " + result.NumberOfFilteredWordsFound);
-            }
-            else
-            {
-                Console.WriteLine(content);
-            }
+            var badWordsRepository = new FilterWordsRepository();
+            var textAnalysisService = new TextFilterService(new FilterWordsProvider(badWordsRepository.LoadBadWordsFromRepo(ConfigSettings.FilterBadWordsRepoFilePath)));
+            var result = textAnalysisService.AnalyseText(content);
+
+
+            Console.WriteLine(args.Length == 0 || args[0].ToLower() != "-disablefilter" ? result.FilteredContent : content);
+
+            Console.WriteLine("Total Number of negative words: " + result.NumberOfFilteredWordsFound);
 
             Console.WriteLine("Press ANY key to exit.");
             Console.ReadKey();
